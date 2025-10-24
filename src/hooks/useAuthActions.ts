df91d09d37299a55abd0616c9ecedf9ee049d914
Anan-Saleh-1995 <anansaleh1995@gmail.com>
2025-10-24 13:57:55 +0300

    Add auth hooks

---
+++ src/hooks/useAuthActions.ts
@@ -0,0 +1,20 @@
+import { useMutation } from '@tanstack/react-query';
+import { signIn, signOut, signUp } from '../services/supabase';
+
+export const useSignIn = () => {
+  return useMutation({
+    mutationFn: ({ email, password }: { email: string; password: string }) =>
+      signIn(email, password),
+  });
+};
+
+export const useSignOut = () => {
+  return useMutation({ mutationFn: signOut });
+};
+
+export const useSignUp = () => {
+  return useMutation({
+    mutationFn: ({ email, password }: { email: string; password: string }) =>
+      signUp(email, password),
+  });
+};
